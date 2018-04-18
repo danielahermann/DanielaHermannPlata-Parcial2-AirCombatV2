3710a2c7be766ce32a8912dc6b626c658d623dc0
DESKTOP-VDNH5UI\Daniela HP <dhermannp@gmail.com>
2018-04-18 16:25:48 -0500

    Version Final Parcial 2

---
+++ DanielaHermannPlata-AirCombatV2/Assets/Scripts/MisilFalling.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MisilFalling : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision collision) { 
+
+		//Escribir el tag tal cual lo escribimos con mayusuculas y todo 
+
+		if (collision.collider.gameObject.tag == "MarsAlien") {
+
+			//Cargamos el prefab de la explosion 
+			GameObject explosion = GameObject.Instantiate (Resources.Load ("Prefabs/MarsExplosion") as GameObject);
+
+			//Muevo la explosion hacia la nave que fue impactada 
+			explosion.transform.position = collision.collider.gameObject.transform.position;
+
+
+			// Destruimos el objeto impactado por el misil 
+			Destroy (collision.collider.gameObject);
+
+			// Destruimos el misil 
+			Destroy(this.gameObject);
+		}
+
+		if (collision.collider.gameObject.tag == "JupiterAlien") {
+			
+			GameObject explosion = GameObject.Instantiate (Resources.Load ("Prefabs/JupiterExplosion") as GameObject);
+			explosion.transform.position = collision.collider.gameObject.transform.position;
+
+
+			// Destruimos el objeto impactado por el misil 
+			Destroy (collision.collider.gameObject);
+
+			// Destruimos el misil 
+			Destroy (this.gameObject);
+		}
+	}
+}
